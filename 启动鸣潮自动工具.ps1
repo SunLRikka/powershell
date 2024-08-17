5c53be0d4b4d8f5627cee8454337a5969f476cfa
SunLRikka <365714384@qq.com>
2024-08-17 19:57:43 +0800

    增加目录变更，处理时更换为只对mkv操作 新增启动鸣潮自动工具

---
+++ 启动鸣潮自动工具.ps1
@@ -0,0 +1,3 @@
+Set-Location D:\Tools\mc_auto_boss
+conda activate mc
+python .\background\main.py
--- 批量转换mkv为mp4.ps1
+++ 批量转换mkv为mp4.ps1
@@ -1,3 +1,4 @@
+Set-Location "D:\library\Videos\OBS Studio"
 $inputFolder = "."
 $outputFolder = ".\converted"
 
@@ -7,11 +8,11 @@ if (!(Test-Path -Path $outputFolder)) {
 }
 
 # 遍历输入文件夹中的所有视频文件
-Get-ChildItem -Path $inputFolder -Include *.mp4,*.avi,*.mov,*.mkv -Recurse | ForEach-Object {
+Get-ChildItem -Path $inputFolder -Include *.mkv -Recurse | ForEach-Object {
     $inputFile = $_.FullName
     $outputFile = Join-Path -Path $outputFolder -ChildPath ([System.IO.Path]::GetFileNameWithoutExtension($_.Name) + ".mp4")
 
     # 使用 FFmpeg 的 CUDA 硬件加速转换,并保持视频和音频编码不变
-    ffmpeg -hwaccel cuda -i $inputFile -c copy $outputFile
+    ffmpeg -hwaccel cuda -i $inputFile -map 0:v:0 -map 0:a:1 -c copy $outputFile
     Write-Host "Converted $inputFile to $outputFile"
 }
